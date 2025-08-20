aa80dc8aab487414ee147df508ea6c288bc9ac7a
Hitesh <hitesh239@gmail.com>
2025-08-20 13:36:46 +0530

    android: EditPost - remove top LinearProgressIndicator on save

--- androidApp/src/main/java/com/ghostly/android/posts/ui/EditPostScreen.kt
+++ androidApp/src/main/java/com/ghostly/android/posts/ui/EditPostScreen.kt
@@ -21,7 +21,6 @@ import androidx.compose.material3.AssistChip
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
@@ -181,10 +180,6 @@ fun EditPostScreen(
                 .padding(horizontal = 16.dp)
                 .padding(bottom = 48.dp)
         ) {
-            if (uiState == EditPostUiState.Saving) {
-                LinearProgressIndicator(modifier = Modifier.fillMaxWidth())
-                Spacer(modifier = Modifier.height(8.dp))
-            }
             // Title field
             OutlinedTextField(
                 value = currentPost?.title ?: "",
